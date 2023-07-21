da0deefd0ffe5f082ae339030a97b589c3706bf5
GinnetR <ginrodriguezrojas@hotmail.com>
2023-07-21 09:02:57 -0500

    Productos-carrusel

--- script/carrusel.js
+++ script/carrusel.js
@@ -1,46 +1,34 @@
-const btnLeft = document.querySelector(".btn-left"),
-  btnRight = document.querySelector(".btn-right"),
-  sliderCarrusel = document.querySelector("#sliderCarrusel"),
-  sliderSection = document.querySelectorAll(".SlidesProducts");
+var product_swiper = new Swiper('.product-swiper', {
+  effect: 'coverflow',
+  grabCursor: false,
+  centeredSlides: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 100,
+    depth: 200,
+    modifier: 2.5,
+    slideShadows:true
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: false,
+  },
+  navigation: {
+    nextEl: '.product-next',
+    prevEl: '.product-prev',
+    hideOnClick: false,
+  },
+});
 
-let operation = 0,
-  counter = 0,
-  widthImg = 100 / sliderSection.length;
+document.querySelector('.product-prev').addEventListener('click', () => {
+  product_swiper.slidePrev();
+});
 
-btnLeft.addEventListener("click", e => moveToLeft());
-btnRight.addEventListener("click", e => moveToRight());
-
-function moveToRight() {
-  if (counter >= sliderSection.length - 1) {
-    counter = 0;
-    operation = 0;
-    sliderCarrusel.style.transform = `translate(-${operation}%)`;
-    sliderCarrusel.style.transition = "none";
-   return }
-    counter++;
-    operation += widthImg;
-    sliderCarrusel.style.transform = `translate(-${operation}%)`;
-    sliderCarrusel.style.transition = "transform 0.6s ease";
-  }
-
-
-function moveToLeft() {
-  counter--;
-  if (counter < 0) {
-    counter = sliderSection.length - 1;
-    operation = widthImg * (sliderSection.length - 1);
-    sliderCarrusel.style.transform = `translate(-${operation}%)`;
-    sliderCarrusel.style.transition = "none";
-  return; } 
-    operation -= widthImg;
-    sliderCarrusel.style.transform = `translate(-${operation}%)`;
-    sliderCarrusel.style.transition = "transform 0.6s ease";
-  }
-
-
-
-
-
+document.querySelector('.product-next').addEventListener('click', () => {
+  product_swiper.slideNext();
+});
   
 function mostrarParte2() {
   document.getElementById("parte1").style.display = "none";
